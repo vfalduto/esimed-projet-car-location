3f8df44fe0c0a11991a141a8ae3b3b83f49dd73f
helian <helian@MacBook-Pro-de-Vincent.local>
2012-07-25 14:54:29 +0200

    added html2pdf lib without bundle structure

---
+++ vendor/html2pdf/lib/Html2pdf/Html2pdf.php
@@ -0,0 +1,8 @@
+<?php
+# vendor/geshi/lib/Geshi/Geshi.php
+
+//require_once __DIR__.'/src/_class/tcpdfConfig.php';
+require_once __DIR__.'/src/html2pdf.class.php';
+
+class Html2pdf_Html2pdf extends HTML2PDF {
+}
